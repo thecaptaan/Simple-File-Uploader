40001de97cd678a88ae51cce3e61e0b2b9fc356d
The Captaan <118829565+thecaptaan@users.noreply.github.com>
2023-04-29 02:27:05 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 "use strict";
+require('dotenv').config();
 const express = require("express");
 const path = require("node:path");
-
+const bodyParser = require('body-parser')
 const server = express();
 
 server.set("view engine", "ejs");
 server.set("views", path.join(__dirname, "src", "views"));
 
 //
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: false }))
 server.use(express.static(path.join(__dirname, "src", "assets", "styles")));
 server.use(express.static(path.join(__dirname, "src", "assets", "images")));
 server.use(express.static(path.join(__dirname, "src", "assets", "javascript")));
@@ -21,5 +24,5 @@ const uploadController = require('./src/upload')
 server.use(uploadController)
 
 server.listen(8080, () => {
-  console.log(`server is listing on localhost::8080`);
+  console.log(`server is listing on http://localhost:8080`);
 });
--- src/upload.js
+++ src/upload.js
@@ -1,6 +1,8 @@
 "use strict";
+const fs = require("node:fs");
 const multer = require("multer");
-const path = require("path");
+const path = require("node:path");
+const jwt = require("jsonwebtoken");
 const uploadController = require("express").Router();
 
 const storage = multer.diskStorage({
@@ -8,26 +10,82 @@ const storage = multer.diskStorage({
     cb(null, "/home/thecaptaan/uploads");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-        "uploader" +
-        uniqueSuffix +
-        path.extname(file.originalname)
-    );
+    const uniqueSuffix = file.fieldname + "_" + Date.now();
+    cb(null, "uploader_" + uniqueSuffix + path.extname(file.originalname));
   },
 });
 
 const upload = multer({ storage: storage }).single("fileuploadinput");
+
 uploadController.post("/upload", (req, res, next) => {
   upload(req, res, (err) => {
-    if (err) {
-      console.log(err)
-      res.send("error");
-    } else {
-      res.redirect('/')
-    }
+    fs.readFile("./token.json", "utf8", function (err, data) {
+      let parsedData = JSON.parse(data);
+      jwt.verify(
+        parsedData.token,
+        process.env.SECRET_TOKEN,
+        function (error, data) {
+          if (error) {
+            if (error.name == "TokenExpiredError") {
+              deleteFile(req.file.path);
+              res.json({
+                success: false,
+                error: "OOPS! Token Expired",
+              });
+            } else {
+              deleteFile(req.file.path);
+              res.json({
+                success: false,
+                error: "OOPS! Token Verification Error",
+              });
+            }
+          } else {
+            if (req.body.uploadToken === parsedData.id) {
+              if (err) {
+                deleteFile(req.file.path);
+                res.json({
+                  success: false,
+                  error: "Unable to process the file.",
+                });
+              } else {
+                fs.rename(
+                  req.file.path,
+                  req.file.destination +
+                    "/" +
+                    "uploader_" +
+                    req.body.studentID +"_"+
+                    Date.now() +
+                    path.extname(req.file.originalname),
+                  (error) => {
+                    if (error) {
+                      deleteFile(req.file.path);
+                      res.json({
+                        success: false,
+                        error: "Unable to process the file.",
+                      });
+                    } else {
+                      res.json({
+                        success: true,
+                      });
+                    }
+                  }
+                );
+              }
+            } else {
+              deleteFile(req.file.path);
+              res.json({
+                success: false,
+                error: "Upload token mismatched",
+              });
+            }
+          }
+        }
+      );
+    });
   });
 });
 
+function deleteFile(filePath) {
+  fs.unlink(filePath, () => {});
+}
 module.exports = uploadController;
